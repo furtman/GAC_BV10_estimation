5d637e61c05a2e31e27177f5c94827f76a523f3d
furtman <70345977+furtman@users.noreply.github.com>
2023-12-29 22:56:48 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -9,11 +9,11 @@
 library(DT)
 library(shiny)
 library(gbm)
-library(ranger)
+#library(ranger)
 library(tidyverse)
 library(shinythemes)
-load(file="final_gbm.rda")
-load(file="final_rf.rda")
+load(file="final_gbm_122923.rda")
+#load(file="final_rf.rda")
 load(file="mp_ratio_tbl.rda")
 load(file="empty_tbl.rda")
 # Define UI for application that draws a histogram
@@ -52,78 +52,94 @@ ui <- shinyUI(
                 tabPanel("About",
                          fluidRow(
                              br(),
-                             p(" This web application is based on a manuscript currently under revision and provide
-                             two machine learning models to estimate early breakthrough of recalcitrant organic MPs in GAC.
-                             Users can easily calculated the BV10s under various conditions by entering a few water quality, MP property, 
-                             and GAC characteristic parameters. Additionally, this web application also provides a function for users to record 
-                             the estimated BV10s along with the corresponding input parameters.
+                             h3("Web-app description:"),
+                             p(" This web application is based on a manuscript 
+                             currently under preparation and provide two machine learning models to estimate early breakthrough of recalcitrant organic MPs in GAC. 
+                             A machine learning model based on gradient boosted machine (GBM) 
+                             is available for estimating bed volume to 10% breakthrough (BV10) of recalcitrant organic MPs. 
+                             Users can easily estimate BV10 values under various conditions by entering a few water quality, 
+                             MP property, and GAC characteristic parameters. The estimated BV10 value is shown on the tab, 
+                             'GBM output'. Additionally, this web-app also provides a function for users to record the estimated BV10s along with the corresponding input parameters. 
+                             The recorded results can then be downloaded as a csv file.
                         
-                        ", style="text-indent: 2em;font-size:20px;")
+                        ", style="text-indent: 2em;font-size:20px;"),
+                             h3("Source:"),
+                             p("This work is subject to GNU GPL v3 license and the source code can be found at:", style="text-indent: 2em;font-size:20px;"),
+                             #br(),
+                             p("https://github.com/furtman/GAC_BV10_estimation.git", style="text-indent: 2em;font-size:20px;"),
+                             #br(),
+                             p("The link to the manuscript is coming soon...", style="text-indent: 2em;font-size:20px;"),
+                             h3("Author Information:"),
+                             p("This project is deveoped by Yoko Koyama (North Carolina State University, herein NCSU), 
+                               Detlef. R. U. Knappe, Ph.D (NCSU), Mohammad A. Khaksar Fasaee (NCSU), Emily Zechman Berglund, Ph.D (NCSU)", style="text-indent: 2em;font-size:20px;"),
+                             h3("Contact:"),
+                             p("Please direct your questions to Yoko Koyama (ykoyama3@ncsu.edu) regarding this project.", style="text-indent: 2em;font-size:20px;")
+                             
                          )),
                 tabPanel("GBM output",
-            ##all about output
-            tags$style("#txt {font-size:30px;
+                         ##all about output
+                         tags$style("#txt {font-size:30px;
                color:blue;
                display:block; }"),
-            tags$style("#bv10_value {font-size:30px;
+                         tags$style("#bv10_value {font-size:30px;
                color:blue;
                display:block;
                bottom: 40px; 
                position:absolute;
                width: 100%;
                left:0px;}"),
-            
-            div(style="text-align:center;
+                         
+                         div(style="text-align:center;
                width:500px;
         height:350px;
         padding-top:70px;
         position:relative;",
-                textOutput("txt"),
-                textOutput("bv10_value"))
-        ),
-        tabPanel("RF output", 
-                 ##all about output
-                 tags$style("#txt2 {font-size:30px;
-               color:blue;
-               display:block; }"),
-                 tags$style("#bv10_value_2 {font-size:30px;
-               color:blue;
-               display:block;
-               bottom: 70px; 
-               position:absolute;
-               width: 100%;
-               left:0px;}"),
-                 
-                 div(style="text-align:center;
-               width:500px;
-        height:350px;
-        padding-top:70px;
-        position:relative;",
-                     textOutput("txt2"),
-                     textOutput("bv10_value_2"))),
-        tabPanel("Results",
-                 fluidRow(
-                     dataTableOutput("bv10s"),
-                     downloadButton("downloadData", "Download"),
-                     p("Download simualtions in csv format. Please make sure the file extension is '.csv'."))
-                 ),
-        tabPanel("Abraham parameter reference", 
-                 fluidRow(
-                     tags$head(tags$style(HTML("a {color: navy;font-size:26px}"))),
-                     h3("Please refer to the link below (UFZ-LSER database v 3.2.1) for Abraham solvation parameters for MPs:"),
-                      
-                     tags$a("https://www.ufz.de",
-                       href="https://www.ufz.de/index.php?en=31698&contentonly=1&m=0&lserd_data[mvc]=Public/start",
-                       style = "font-size:20pt"),
-                      br(),
-                     h3("About the Six Abraham parameters:"),
-                      
-                     p(" Abraham solvation parameters were developed by Abraham et al (2004) to parameterize 
+                             textOutput("txt"),
+                             textOutput("bv10_value"))
+                ),
+        #         tabPanel("RF output", 
+        #                  ##all about output
+        #                  tags$style("#txt2 {font-size:30px;
+        #        color:blue;
+        #        display:block; }"),
+        #                  tags$style("#bv10_value_2 {font-size:30px;
+        #        color:blue;
+        #        display:block;
+        #        bottom: 70px; 
+        #        position:absolute;
+        #        width: 100%;
+        #        left:0px;}"),
+        #                  
+        #                  div(style="text-align:center;
+        #        width:500px;
+        # height:350px;
+        # padding-top:70px;
+        # position:relative;",
+        #                      textOutput("txt2"),
+        #                      textOutput("bv10_value_2"))),
+                tabPanel("Results",
+                         fluidRow(
+                             dataTableOutput("bv10s"),
+                             downloadButton("downloadData", "Download"),
+                             p("Download simualtions in csv format. Please make sure the file extension is '.csv'."))
+                ),
+                tabPanel("Abraham parameter reference", 
+                         fluidRow(
+                             tags$head(tags$style(HTML("a {color: navy;font-size:26px}"))),
+                             h3("Please refer to the link below (UFZ-LSER database v 3.2.1) for Abraham solvation parameters for MPs:"),
+                             
+                             tags$a("https://www.ufz.de",
+                                    href="https://www.ufz.de/index.php?en=31698&contentonly=1&m=0&lserd_data[mvc]=Public/start",
+                                    style = "font-size:20pt"),
+                             br(),
+                             h3("About the Six Abraham parameters:"),
+                             
+                             p(" Abraham solvation parameters were developed by Abraham et al (2004) to parameterize 
                        the intermolecular forces between solutes and solvents and are applicable to gas, liquid, and solid phases.",
-                        style="text-indent: 2em;font-size:20px;"),
-                    
-                      
-                     p(" The meaning of these six parameters follows.
+                               style="text-indent: 2em;font-size:20px;"),
+                             
+                             
+                             p(" The meaning of these six parameters follows.
                         A represents the hydrogen bonding acidity of solute; 
                         B represents the hydrogen bonding basicity; S represents the polirazability of solute;
                         L represents the partitioning coefficient between gas phase and hexadecane;
@@ -131,12 +147,12 @@ ui <- shinyUI(
                         E represents the excessive molar refraction of solute.
                         
                         ", style="text-indent: 2em;font-size:20px;")
-                     )),
-        tabPanel("GAC property references", 
-                 fluidRow(
-                h3("Please refer to this table if you have not measured your GAC characteristics."),
-                 tableOutput("table")))
-        )
+                         )),
+                tabPanel("GAC property references", 
+                         fluidRow(
+                             h3("Please refer to this table if you have not measured your GAC characteristics."),
+                             tableOutput("table")))
+            )
         )
         )
     )
@@ -146,41 +162,47 @@ ui <- shinyUI(
 # Define server logic 
 server <- function(input, output,session) {
     ntext <- eventReactive(input$goButton, {
-        newdt<-data.frame(DOC=input$DOC, pH=input$pH, UV254=input$UV254, C0=input$C0, charge=input$charge,
+        newdt<-data.frame(DOC=input$DOC, pH=input$pH, 
+                          #UV254=input$UV254, 
+                          C0=input$C0, charge=input$charge,
                           L=input$L, B=input$B,E=input$E, S=input$S, A=input$A, V=input$V,
                           BET=input$BET, pzc=input$pzc, mp_ratio=input$mp_ratio, EBCT=input$EBCT, CD=0,PD=0)
         logbv10<-predict(final_gbm ,newdt)
         predicted<-10^logbv10
-       
+        
         return(predicted)
     }
     
     )
-    ntext2 <- eventReactive(input$goButton, {
-        newdt<-data.frame(DOC=input$DOC, pH=input$pH, UV254=input$UV254, C0=input$C0, charge=input$charge,
-                          L=input$L, B=input$B,E=input$E, S=input$S, A=input$A, V=input$V,
-                          BET=input$BET, pzc=input$pzc, mp_ratio=input$mp_ratio, EBCT=input$EBCT, CD=0,PD=0)
-        logbv10_2<-predict(final_rf ,newdt)$predictions
-        predicted2<-10^logbv10_2
-        return(predicted2)}
-    )
+    # ntext2 <- eventReactive(input$goButton, {
+    #     newdt<-data.frame(DOC=input$DOC, pH=input$pH, 
+    #                       #UV254=input$UV254, 
+    #                       C0=input$C0, charge=input$charge,
+    #                       L=input$L, B=input$B,E=input$E, S=input$S, A=input$A, V=input$V,
+    #                       BET=input$BET, pzc=input$pzc, mp_ratio=input$mp_ratio, EBCT=input$EBCT, CD=0,PD=0)
+    #     logbv10_2<-predict(final_rf ,newdt)$predictions
+    #     predicted2<-10^logbv10_2
+    #     return(predicted2)}
+    # )
     
     #add row upon click
     values <- reactiveValues()
     empty_tbl->result
     values$df <- result
-     observeEvent(input$goButton2, {
-        newdt<-data.frame(DOC=input$DOC, pH=input$pH, UV254=input$UV254, C0=input$C0, charge=input$charge,
+    observeEvent(input$goButton2, {
+        newdt<-data.frame(DOC=input$DOC, pH=input$pH,
+                          #UV254=input$UV254, 
+                          C0=input$C0, charge=input$charge,
                           L=input$L, B=input$B,E=input$E, S=input$S, A=input$A, V=input$V,
                           BET=input$BET, pzc=input$pzc, mp_ratio=input$mp_ratio, EBCT=input$EBCT, CD=0,PD=0)
         logbv10<-predict(final_gbm ,newdt)
         GBM_bv10<-round(10^logbv10,digits=0)
-        logbv10_2<-predict(final_rf ,newdt)$predictions
-        RF_bv10<-round(10^logbv10_2,digits=0)
-        newRow<-cbind(newdt,GBM_bv10,RF_bv10)
+        # logbv10_2<-predict(final_rf ,newdt)$predictions
+        # RF_bv10<-round(10^logbv10_2,digits=0)
+        newRow<-cbind(newdt,GBM_bv10)
         #colnames(newRow)[c(16,17)]<-c("GBM_BV10","RF_BV10")
         values$df <- rbind(values$df,newRow)
-     
+        
     }  )
     
     output$txt<-renderText({
@@ -192,9 +214,9 @@ server <- function(input, output,session) {
     output$txt2<-renderText({
         paste( "Estimated BV10 by random forest (RF) model is " )
     })
-    output$bv10_value_2<-renderText({
-        ntext2()
-    })
+    # output$bv10_value_2<-renderText({
+    #     ntext2()
+    # })
     output$table <- renderTable(mp_ratio_tbl)
     
     #thedata <- reactive(iris)
